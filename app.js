85e9321e74faa3aafa673ce5400b710512e838c3
t6767@mail.ru <t6767@mail.ru>
2020-01-05 15:56:31 +0600

    nm

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(exports.myDateTime);
+  res.end(exports.myDateTime());
 }).listen(8080);
