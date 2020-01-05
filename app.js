b9a09b851363f2950b4e5b810d831ed62e32053b
t6767@mail.ru <t6767@mail.ru>
2020-01-05 18:44:55 +0600

    uy

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var http = require('http');
-var dt = require('./myfirstmodule');
-
+var fs = require('fs');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.end();
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
 }).listen(8080);
