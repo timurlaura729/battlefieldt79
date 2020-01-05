e2c12e9aa39a7f01ca0aa8099a99f8b5c42013d1
t6767@mail.ru <t6767@mail.ru>
2020-01-05 15:58:50 +0600

    гз

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-exports.myDateTime = function () {
-  return "7777999999";
-};
-
-
 var http = require('http');
+var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(exports.myDateTime());
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
 }).listen(8080);
