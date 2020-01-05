1fc4c6742e9203308f5f31cd56c09e9b67ae68b4
t6767@mail.ru <t6767@mail.ru>
2020-01-05 10:56:27 +0600

    hg

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello WorldXXXXX!');
+  res.end('Hello WorldXXXXX111777799999!');
 }).listen(8080);
