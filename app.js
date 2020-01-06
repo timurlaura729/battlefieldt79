117004278bb16446c8764666e110ab2277dd9466
t6767@mail.ru <t6767@mail.ru>
2020-01-06 12:41:10 +0600

    нг

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 var url = require('url');
-var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
-var q = url.parse(adr, true);
-
-console.log(q.host); //returns 'localhost:8080'
-console.log(q.pathname); //returns '/default.htm'
-console.log(q.search); //returns '?year=2017&month=february'
-
-var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
-console.log(qdata.month); //returns 'february'
-
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  var qx = req.url.parse(adr, true);
-  res.write(req.url);
+  var qx = url.parse(req.url, true);
+  res.write("url "+req.url);
   res.write("<br>");
-  res.write(qx.search);
+  res.write("host "+qx.host); 
   res.write("<br>");
-
-
+  res.write("pathname "+qx.pathname); 
+  res.write("<br>");
+  res.write("get "+qx.search);
+  res.write("<br>");
+/*
+Выдаст
+url /?ttt=7777
+host null
+pathname /
+get ?ttt=7777
+*/	
+	var qdataxxx = qx.query; 
+	res.write("<br>");
+    res.write(qdataxxx.ttt);
+// если не будет парамметра обекта будет ошибка  
+	
+  var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+  var q = url.parse(adr, true);
+  res.write("<h1>TEST</h1>"+q.host); //returns 'localhost:8080'
+  res.write("<br>");
+  res.write(q.pathname); //returns '/default.htm'
+  res.write("<br>");
+  res.write(q.search); //returns '?year=2017&month=february'	
+  
+  var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+  res.write("<br>");
   res.write(qdata.month);
   res.write("<br>");
   res.write(qdata.year);
