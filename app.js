6e619a57372daa43c0dce9339b116508b48c88b3
t6767@mail.ru <t6767@mail.ru>
2020-01-06 10:24:32 +0600

    didish

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
+fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+
+  fs.readFile('mynewfile1.txt', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
