3be68578ebeec9187575e35b809d0782c96d5102
t6767@mail.ru <t6767@mail.ru>
2020-01-06 12:52:02 +0600

    гз

--- app.js
+++ app.js
@@ -1,41 +1,17 @@
-var url = require('url');
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var qx = url.parse(req.url, true);
-  res.write("url "+req.url);
-  res.write("<br>");
-  res.write("host "+qx.host); 
-  res.write("<br>");
-  res.write("pathname "+qx.pathname); 
-  res.write("<br>");
-  res.write("get "+qx.search);
-  res.write("<br>");
-/*
-Выдаст
-url /?ttt=7777
-host null
-pathname /
-get ?ttt=7777
-*/	
-	var qdataxxx = qx.query; 
-	res.write("<br>");
-    res.write(qdataxxx.ttt);
-// если не будет парамметра обекта будет ошибка  
-	
-  var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
-  var q = url.parse(adr, true);
-  res.write("<h1>TEST</h1>"+q.host); //returns 'localhost:8080'
-  res.write("<br>");
-  res.write(q.pathname); //returns '/default.htm'
-  res.write("<br>");
-  res.write(q.search); //returns '?year=2017&month=february'	
-  
-  var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
-  res.write("<br>");
-  res.write(qdata.month);
-  res.write("<br>");
-  res.write(qdata.year);
-  res.write("<br>");
-  res.end();
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
 }).listen(8080);
