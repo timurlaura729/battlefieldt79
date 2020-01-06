2f0d244be92e3417154677ef57d24a16e41f66b4
t6767@mail.ru <t6767@mail.ru>
2020-01-06 12:20:41 +0600

    up

--- app.js
+++ app.js
@@ -13,5 +13,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(req.url);
+  res.write(qdata.month);
   res.end();
 }).listen(8080);
