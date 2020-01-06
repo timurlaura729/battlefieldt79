4fafc7877da4c609c471dc5ff3884863ad9ee0ad
t6767@mail.ru <t6767@mail.ru>
2020-01-06 12:18:53 +0600

    uy

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ console.log(q.search); //returns '?year=2017&month=february'
 
 var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
 console.log(qdata.month); //returns 'february'
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(req.url);
+  res.end();
+}).listen(8080);
