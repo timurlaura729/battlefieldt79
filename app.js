d6b1f9ff550f7e510bd0ab25baf4ef5ddc41ef89
t6767@mail.ru <t6767@mail.ru>
2020-01-06 12:24:23 +0600

    y

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(req.url);
   res.write("<br>");
+  res.write(req.search);
+  res.write("<br>");
   res.write(qdata.month);
   res.write("<br>");
   res.write(qdata.year);
