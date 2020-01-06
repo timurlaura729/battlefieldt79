29797a1393888025e5499f4fc89e30a6f710a1e9
t6767@mail.ru <t6767@mail.ru>
2020-01-06 12:26:20 +0600

    up

--- app.js
+++ app.js
@@ -12,10 +12,13 @@ console.log(qdata.month); //returns 'february'
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+  var qx = req.url.parse(adr, true);
   res.write(req.url);
   res.write("<br>");
-  res.write(req.search);
+  res.write(qx.search);
   res.write("<br>");
+
+
   res.write(qdata.month);
   res.write("<br>");
   res.write(qdata.year);
